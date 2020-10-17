4c06ac638fc88175aa75bd04cd47830f14dd9417
pr0grara <azbaghda@gmail.com>
2020-10-17 01:33:03 -0700

    add multiple user handling and much better comment judging logic

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # facebook_army
+### from terminal
+0. ### npm install (chromium should take about a minute)
+### inside autoLike.js
+1. ### add your account credentials in the "users" object
+2. ### add a target url in the "targetUrl" variable 
+#### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**NOTE facebook has many different types of content, autoLike will currently** 
+#### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**only work on posts [like this](https://www.facebook.com/watch/?v=257467945464976&external_log_id=a60e280e-1708-4a4f-96ec-841d329d6426&q=kitten)** 
+3. ### run autoLike.js
+###### &nbsp;&nbsp;
+# autolike will run the main function for every user of the "users" object  
+## autoLike will open a new browser instance inside chromium from which it will:
+1. ### navigate to facebook login
+2. ### enter user credentials and submit
+3. ### navigate to the target post
+4. ### Open comments
+5. ### Click on the "See More Comments" button N times
+6. ### Iterate through every comment and click "See More" where applicable
+7. ### Iterate again through every comment and run the text through our check functions
+8. ### Any comment found to be favorable will then be "liked"
+9. ### After completion the console will report number of likes given and the browser will be closed
+10. ### This process will be repeated for every user in "users" object
+&nbsp;&nbsp;
+#### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; *godspeed general*
--- autoLike.js
+++ autoLike.js
@@ -1,140 +1,207 @@
 const users = {
-  demo: { username: "DEMO USER", password: "AliyevEatsDonkeyAss" }, //enter credentials here
+  0: { username: "demo@user.com", password: "123456" }, //enter credentials here
+  1: { username: "demo@user.com", password: "123456" }, //every account added to this object will be run through program
 };
 
-const hashtags = {
-  artStrong: "#ArtsakhStrong",
-  artIsArm: "#ArtsakhIsArmenia",
-  stopAli: "#StopAliyev",
-  sancAzer: "#SanctionAzerbaijan",
-  sancTurk: "#SanctionTurkey",
-  recArt: "#RecognizeArtsakh",
-  arm: "#Armenia",
-  believe: "#BelieveArmenia",
-  azeriLies: "#AzerbaijanLies",
+const targetUrl = ''; //input url of target post here
+
+//after completing above inputs you may now run autoLike.js 
+
+const regexHashes = [
+  new RegExp("#recognizeartsa", "gi"),
+  new RegExp("#sanctionturk", "gi"),
+  new RegExp("#stopazer", "gi"),
+  new RegExp("#stopaliyev", "gi"),
+  new RegExp("#stoperdo", "gi"),
+  new RegExp("#stopturkey", "gi"),
+  new RegExp("#artsakhstrong", "gi"),
+];
+
+const trickTrapsPro = [
+  new RegExp("kh is arm", "gi"),
+  new RegExp("stop aliyev", "gi"),
+  new RegExp("stop azer", "gi"),
+  new RegExp("stop erdog", "gi"),
+  new RegExp("stop turkey", "gi"),
+  new RegExp("ancestral homeland", "gi"),
+];
+
+const trickTrapsCon = [
+  new RegExp("kh is az", "gi"),
+  new RegExp("khojaly", "gi"),
+  new RegExp("occupied territories", "gi"),
+  new RegExp("not disput", "gi"),
+  new RegExp("armenian terror", "gi"),
+  new RegExp("internationally rec", "gi"),
+  new RegExp("not contest", "gi"),
+  new RegExp("must withdraw", "gi"),
+];
+
+const flagExp = [
+  new RegExp("🇦🇲", "gi"),
+  new RegExp("🇦🇿", "gi"),
+  new RegExp("🇹🇷", "gi"),
+];
+
+const flagCheck = (com) => {
+  var armoFlag = false;
+  var azeriFlag = false;
+  var turkFlag = false;
+
+  if (com.match(flagExp[0])) armoFlag = true;
+  if (com.match(flagExp[1])) azeriFlag = true;
+  if (com.match(flagExp[2])) turkFlag = true;
+
+  if (armoFlag && (azeriFlag || turkFlag)) return -1;
+  if (azeriFlag || turkFlag) return -2;
+  if (armoFlag) return 1;
+  return 0;
+};
+
+const containsHash = (com) => {
+  return regexHashes.some((regex) => com.match(regex));
+};
+
+const trickerTraper = (com) => {
+  if (trickTrapsCon.some((trick) => com.match(trick))) return false;
+  if (trickTrapsPro.some((trick) => com.match(trick))) return true;
+};
+
+const nameCheck = (name) => {
+  lastThree = name.slice(name.length - 3);
+  if (lastThree === "ian" || lastThree === "yan") return true;
+};
+
+const isProArmenian = (com) => { 
+  //here we run through an obstacle course of judgement
+  //we look at 1.flags 2.hashtags 3.phraseology and 4.commentors last name
+  //if at any one of these stops we determine the comment is proArmenian we return true and the comment is liked
+  var lastName = com.split('\n')[0].split(' ')[1];
+  console.log(lastName);
+  if (flagCheck(com) > 0) return true;
+  if (containsHash(com)) return true;
+  if (trickerTraper(com)) return true; //here sum magic ;)
+  if (nameCheck(lastName)) return true; //good to b armo :) this function puts in WORK
+  return false;
 };
 
 const puppeteer = require("puppeteer");
-const username = 'Adam Melks'; //specify user here
-const password = '$kake-geradzek!735'; //and here
-// const username = users.demo.username; //specify user here
-// const password = users.demo.password; //and here
-
-let browser = null;
-let page = null;
 
 try {
   (async () => {
-    browser = await puppeteer.launch({ headless: false });
-
-    page = await browser.newPage();
-    page.setViewport({
-      width: 1280,
-      height: 800,
-      isMobile: false,
-    });
-
-    await page.goto("https://facebook.com/login", {
-      waitUntil: "networkidle2",
-    });
-
-    //LOGIN
-    await page.type('input[id="email"]', username, { delay: 25 });
-    await page.type('input[id="pass"]', password, { delay: 25 });
-    await page.click('button[id="loginbutton"]');
-
-    //NAVIGATE TO POST AND OPEN COMMENTS
-    await page.waitFor(2000);
-    await page.goto("https://www.facebook.com/watch/?v=401016897697280");
-    await page.waitFor(2000);
-    await page.keyboard.press("Escape");
-    try {
-      await page.click('div[class="pfnyh3mw j83agx80 bp9cbjyn"]');
-    } catch {
-      console.log("View Comments Error");
-    }
-    await page.waitFor('div[role = "article"]');
-
-    //CLICK 'See More Comments' i TIMES
-    var seeMoreComments;
-    for (let i = 0; i < 10; i++) {
-      //iterates over entire post i times
-      seeMoreComments = await page.$$(
-        'span[class="j83agx80 fv0vnmcu hpfvmrgz"]'
-      ); //grabs the 'see more comments' button
-
-      var idx = seeMoreComments.length - 1;
+    for (let a = 0; a < Object.values(users).length; a++) {
+      console.log(`initiating protocol for user ${users[a].username}`)
+      let browser = null;
+      let page = null;
+      var username = users[a].username; 
+      var password = users[a].password; 
+      browser = await puppeteer.launch({ headless: false });
+  
+      page = await browser.newPage();
+      page.setViewport({
+        width: 1280,
+        height: 800,
+        isMobile: false,
+      });
+  
+      await page.goto("https://facebook.com/login", {
+        waitUntil: "networkidle2",
+      });
+  
+      //LOGIN
+      await page.type('input[id="email"]', username, { delay: 50 });
+      await page.type('input[id="pass"]', password, { delay: 50 });
+      await page.waitFor(1000);
+      await page.click('button[id="loginbutton"]');
+  
+      //NAVIGATE TO TARGET POST AND OPEN COMMENTS
+      await page.waitFor(6000);
+      await page.goto(targetUrl);
+      await page.waitFor(2000);
+      await page.keyboard.press("Escape");
       try {
-        await seeMoreComments[idx].click();
-        await page.waitFor(2000);
+        await page.click('div[class="pfnyh3mw j83agx80 bp9cbjyn"]');
       } catch {
-        console.log("'See More Comments' error");
+        console.log("View Comments Error");
       }
-    }
-
-    //EXPAND ALL COMMENTS
-    var j = 0; //comment counter
-    const comments = await page.$$('div[role="article"]');
-    while (comments.length > j) {
-      //makes sure we process all new comments revealed after clicking 'see more comments'
-      const buttons = await comments[j].$$('div[role="button"]');
-      const buttonsProperties = await Promise.all(
-        buttons.map((button) => button.getProperty("innerText"))
-      );
-      const buttonsJSON = await Promise.all(
-        buttonsProperties.map((prop) => prop.jsonValue())
-      );
-
-      for (let k = 0; k < buttons.length; k++) {
-        //iterates over each comments buttons looking for 'See More'
-        if (buttonsJSON[k] === "See More") {
-          try {
-            await buttons[k].click();
-            await page.waitFor(300);
-          } catch {
-            console.log("Expanding Comments Error");
-          }
-        }
-      }
-      j++;
-    }
-
-    //LIKE ALL COMMENTS WITH OUR #
-    j = 0; //reset comment counter
-    var likedCount = 0;
-    while (comments.length > j) {
-      console.log(j);
-      console.log(comments.length);
-      const comment = comments[j];
-      const elementText = await comment.getProperty("innerText");
-      const innerText = await elementText.jsonValue();
-      const textArr = innerText.split(" ");
-      // console.log(textArr);
-      const regex = new RegExp('#recognizeartsakh', 'gi');
-
-      if (
-        textArr.some((word) => !!word.match(regex)) 
-      ) {
-        await page.waitFor(1000);
-        console.log("BINGO");
+      await page.waitFor('div[class="l9j0dhe7 tkr6xdv7 buofh1pr eg9m0zos"]');
+      await page.waitFor(1000);
+  
+      //CLICK 'See More Comments' i TIMES
+      var seeMoreComments;
+      const posts = await page.$$('div[class="l9j0dhe7 tkr6xdv7 buofh1pr eg9m0zos"]')
+      
+      for (let i = 0; i < 20; i++) { //iterates over entire post i times
+        seeMoreComments = await posts[0].$$('div[role="button"]'); //grabs buttons from first post
+        var idx = seeMoreComments.length - 1;
+        
         try {
-          var like = await comment.$$('div[aria-label="Like"]'); //it can be a div
-          if (like.length === 0) like = await comment.$$('a[aria-label="Like"]') //or a link
-          if (like.length > 0) { //make sure the node exists (aka we haven't already liked comment)
-            console.log("!");
-            await like[0].click();
-            await page.waitFor(1000);
-            likedCount++;
-          }
-        } catch (e) {
-          console.log(e);
+          await seeMoreComments[idx].click();
+          await page.waitFor(2000);
+        } catch {
+          console.log("'See More Comments' error");
         }
       }
-      console.log("---");
-      j++;
-    }
-    console.log(`finished liking ${likedCount + (likedCount === 1 ? ' comment' : ' comments')}`);
-  })();
-} catch (e) {
-  console.log(e);
-}
+  
+      //EXPAND ALL COMMENTS
+      var j = 0; //comment counter
+      const comments = await page.$$('div[role="article"]');
+      while (comments.length > j) { //makes sure we process all new comments revealed after clicking 'see more comments'
+        const buttons = await comments[j].$$('div[role="button"]');
+        const buttonsProperties = await Promise.all(
+          buttons.map((button) => button.getProperty("innerText"))
+        );
+        const buttonsJSON = await Promise.all(
+          buttonsProperties.map((prop) => prop.jsonValue())
+        );
+  
+        for (let k = 0; k < buttons.length; k++) { //iterates over each comments buttons looking for 'See More'
+          if (buttonsJSON[k] === "See More") {
+            try {
+              await buttons[k].click();
+              await page.waitFor(600);
+            } catch {
+              console.log("Expanding Comments Error");
+            }
+          }
+        }
+        j++;
+      }
+  
+      //LIKE ALL COMMENTS THAT PASS isProArmenian()
+      j = 0; //reset comment counter
+      var likedCount = 0;
+
+      while (comments.length > j) {
+        console.log(`${j}/${comments.length}`);
+        const comment = comments[j];
+        const elementText = await comment.getProperty("innerText");
+        const innerText = await elementText.jsonValue();
+        
+        if (isProArmenian(innerText)) {
+          await page.waitFor(2000);
+          // console.log("BINGO");
+          try {
+            var like = await comment.$$('div[aria-label="Like"]'); //it can be a div
+            if (like.length === 0)
+              like = await comment.$$('a[aria-label="Like"]'); //or a link
+            if (like.length > 0) { //make sure the node exists (aka we haven't already liked this comment and query selector came up empty)
+              // console.log("!");
+              await like[0].click();
+              await page.waitFor(1000 + Math.floor(Math.random() * 3000));
+              likedCount++;
+            }
+          } catch (e) {
+            console.log(e);
+          }
+        }
+        // console.log("---");
+        j++;
+      }
+      console.log(`finished liking ${likedCount + (likedCount === 1 ? ' comment' : ' comments')} for ${username}`);
+      browser.close();
+    }})();
+  } catch (e) {
+    console.log(e);
+    browser.close();
+  }
